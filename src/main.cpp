3133aef92c44426468f771dcb7bd679339234b66
Febryan Grady <febryan.grady@binus.edu>
2023-11-09 05:57:20 +0700

    MQTT STUFF BLYAT

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,7 @@
 #include <Arduino.h>
 #include "handler/Wifi_Handler.hpp"
+#include <Arduino_MQTT_Client.h>
+#include <time.h>
 
 #define SERIAL_PORT 9600
 
@@ -8,8 +10,8 @@
     #define WIFI_PASSWORD "Ade1234567890"
 #pragma endregion
 #pragma region MODULES
-    #define PIN_BUZZER 1
-    #define PIN_
+    #define PIN_BUZZER 22
+    #define PIN_SENSOR 2
 #pragma endregion
 #pragma region INIT_FUNCTION
     void init_wifi();
@@ -23,7 +25,10 @@ void setup() {
 }
 
 void loop() {
-    
+    time_t t;
+    t = time(NULL);
+    tm* t_ptr = localtime(&t);
+    Serial.println(asctime(t_ptr));
 }
 
 
@@ -36,6 +41,7 @@ void init_wifi(){
 }
 
 void init_modules(){
-    
+    pinMode(PIN_SENSOR, INPUT);
+    pinMode(PIN_BUZZER, OUTPUT);
 }
 
